010cdace260435ce57da36ddd9b7f3303d1ab581
shiny <kindlyman343423@gmail.com>
2023-07-11 11:33:23 +0200

    Remove unused code like bootstrapper and service container

--- src/bounded_context/infrastructure/http/mod.rs
+++ src/bounded_context/infrastructure/http/mod.rs
@@ -1,4 +1,3 @@
-pub mod tmp_controller;
 pub mod index_controller;
 pub mod create_task_controller;
 pub mod configure_routes;
--- src/bounded_context/infrastructure/mod.rs
+++ src/bounded_context/infrastructure/mod.rs
@@ -1,5 +1,3 @@
 pub mod config;
 pub mod http;
 pub mod mysql;
-pub mod service_container;
-pub mod bootstrapper;
