20fae46fa2693a619fa0b85204f62d86ab9a6096
shiny <kindlyman343423@gmail.com>
2023-07-12 12:51:28 +0200

    Refactor on task status to not use magic constants

--- src/bounded_context/domain/task_status.rs
+++ src/bounded_context/domain/task_status.rs
@@ -14,9 +14,9 @@ pub enum TaskStatus {
 impl fmt::Display for TaskStatus {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            TaskStatus::Todo => write!(f, "Todo"),
-            TaskStatus::InProgress => write!(f, "InProgress"),
-            TaskStatus::Done => write!(f, "Done"),
+            TaskStatus::Todo => write!(f, "{}", TODO_VALUE),
+            TaskStatus::InProgress => write!(f, "{}", IN_PROGRESS_VALUE),
+            TaskStatus::Done => write!(f, "{}", DONE_VALUE),
         }
     }
 }
@@ -29,5 +29,25 @@ impl TaskStatus {
             DONE_VALUE => Some(TaskStatus::Done),
             &_ => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display() {
+        assert_eq!(format!("{}", TaskStatus::Todo), TODO_VALUE);
+        assert_eq!(format!("{}", TaskStatus::InProgress), IN_PROGRESS_VALUE);
+        assert_eq!(format!("{}", TaskStatus::Done), DONE_VALUE);
+    }
+
+    #[test]
+    fn test_from_string() {
+        assert_eq!(TaskStatus::from_string(TODO_VALUE), Some(TaskStatus::Todo));
+        assert_eq!(TaskStatus::from_string(IN_PROGRESS_VALUE), Some(TaskStatus::InProgress));
+        assert_eq!(TaskStatus::from_string(DONE_VALUE), Some(TaskStatus::Done));
+        assert_eq!(TaskStatus::from_string("InvalidStatus"), None);
     }
 }
